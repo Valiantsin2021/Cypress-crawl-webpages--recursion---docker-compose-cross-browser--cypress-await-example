289a2520fea607aed171c65666d10d03032fe73f
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2025-02-09 12:42:42 +0100

    update crawl test

--- cypress/e2e/crawl_bahmutov_page.cy.js
+++ cypress/e2e/crawl_bahmutov_page.cy.js
@@ -38,3 +38,26 @@ describe('Crawl Bahmutov blog pages', () => {
     })
   })
 })
+// describe('Crawl Bahmutov blog pages', () => {
+//   it('Visit every link from the repos list', () => {
+//     cy.visit('https://glebbahmutov.com/blog/index.html')
+//     const links = []
+//     cy.get('[itemprop="name"] a')
+//       .should('have.length', 100)
+//       .then($links => {
+//         links.push(
+//           ...$links.toArray().map(link => {
+//             const obj = {
+//               title: link.innerHTML,
+//               link: `https://glebbahmutov.com/blog${link.getAttribute('href')}`
+//             }
+//             return obj
+//           })
+//         )
+//       })
+//     cy.wrap(links).then(links => {
+//       cy.writeFile('links.json', links)
+//       console.log(JSON.stringify(links, null, 2))
+//     })
+//   })
+// })
